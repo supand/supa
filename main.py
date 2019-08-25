a1ae2b25186a978e0df491274274ae4d95f4f494
supan <supand007@gmail.com>
2019-08-25 22:21:41 +0400

    added account 104.51

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ import csv
 
 
 def parse(fname, letter, nc, pc):
-    f = open('c:/Users/omgbe/Documents/smeta/{}.{}.csv'.format(fname, letter), 'r')
+    f = open('c:/Users/supan/Documents/smeta/{}.{}.csv'.format(fname, letter), 'r')
     r = csv.reader(f, quotechar='"', delimiter=',',quoting=csv.QUOTE_ALL, skipinitialspace=True)
 
     a = {}
     for row in r:
         name = row[nc].strip()
-        price = float(row[pc].replace(',', '.'))
-        key = (name, price)
-        if name == "":
+        if name == "" or row[pc] == '-':
             continue
+
+        print(row[pc])
+        price = float(row[pc].replace(',', '.').replace(' ', '').replace('\xa0', ''))
+        key = (name, price)
+
 
         if key in a:
             a[key] = a[key] + 1
@@ -41,20 +44,22 @@ def compare(fname, a, b):
             c[key_b] = {'qnt': 2, 'comments': ''}
 
     return c
-
-accs = [('108.51', 1, 5, 7, 14),
-        ('108.52', 1, 5, 7, 14),
-        ('108.55', 1, 5, 7, 14),
-        ('108.56', 1, 5, 7, 14),
+#       filename, aName,aPrice,bName,bPrice
+accs = [
+    ('108.51', 1, 5, '108.51', 7, 14),
+    ('108.52', 1, 5, '108.52', 7, 14),
+    ('108.55', 1, 5, '108.55', 7, 14),
+    ('108.56', 1, 5, '108.56', 7, 14),
+    ('108.51', 1, 7, '104.51', 7, 15)
         ]
-f = open('c:/Users/omgbe/Documents/smeta/108.C.csv', 'w')
+f = open('c:/Users/supan/Documents/smeta/result.C.csv', 'w')
 for acc in accs:
     print("processing {} account".format(acc[0]))
     a = parse(acc[0],'A', acc[1], acc[2])
-    b = parse(acc[0],'B', acc[3], acc[4])
+    b = parse(acc[3],'B', acc[4], acc[5])
     c = compare(acc[0], a, b)
 
     for key_c in c:
-        f.write('{}\t{}\t{}\t{}\t{}\n'.format(acc[0],key_c[0], key_c[1], c[key_c]['qnt'], c[key_c]['comments']))
+        f.write('{}\t{}\t{}\t{}\t{}\n'.format(acc[3],key_c[0], key_c[1], c[key_c]['qnt'], c[key_c]['comments']))
 
 f.close()

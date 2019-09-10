adcd87f68db38b003c7d810c1bf8fdcf9415fa39
supan <supand007@gmail.com>
2019-09-10 10:41:27 +0400

    check quantity

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def parse(fname, letter, nc, pc):
     a = {}
     for row in r:
         # print('{} records'.format(row))
-        name = row[nc].strip()
+        name = row[nc].strip().replace('\xa0', ' ')
         name = name.strip(', ')
         name = name.strip(',')
         if name == "" or row[pc] == '-':
@@ -30,49 +30,65 @@ def parse(fname, letter, nc, pc):
 def compare(fname, a, b):
     c = {}
     for key_a in a:
-        # print("'A: {}'".format(key_a))
-
+        qa = a[key_a]
         if key_a in b:
-            qa = a[key_a]
             qb = b[key_a]
             if qa == qb:
-                c[key_a] = {'qnt': 1, 'comments': ''}
+                c[key_a] = {'qnt': 1, 'comments': '', 'qa': qa, 'qb': qb}
             elif qa > qb:
-                c[key_a] = {'qnt': 0, 'comments': 'добавить в смету {}'.format(qa - qb)}
+                c[key_a] = {'qnt': 0, 'comments': 'добавить в смету {}'.format(qa - qb), 'qa': qa, 'qb': qb}
             else:
-                c[key_a] = {'qnt': 2, 'comments': 'убрать из сметы {}'.format(qb - qa)}
+                c[key_a] = {'qnt': 2, 'comments': 'убрать из сметы {}'.format(qb - qa), 'qa': qa, 'qb': qb}
         else:
-            c[key_a] = {'qnt': 0, 'comments': ''}
+            c[key_a] = {'qnt': 0, 'comments': '', 'qa': qa, 'qb': 0}
 
     for key_b in b:
-        # print("'B: {}'".format(key_b))
+        qb = b[key_b]
         if key_b not in a:
-            c[key_b] = {'qnt': 2, 'comments': ''}
+            c[key_b] = {'qnt': 2, 'comments': '', 'qa': 0, 'qb': qb}
 
     return c
 #       filename, aName,aPrice,bName,bPrice
 
 def fix_doubles(d):
     res = {}
+    cc = d.copy()
+    to_del = None
     for key in d:
         res[key] = d[key]
         if d[key] == 1:
             continue
 
-        for key_1 in d:
+        for key_1 in cc:
+            if d[key] == 1:
+                continue
+
             if key == key_1:
                 continue
 
             (name, price) = key
             (name_1, price_1) = key_1
-            if price == price_1 and (name in name_1 or name_1 in name):
-                print('rewrite {} to {}'.format(key, key_1))
-                res[(name, price)] = {'qnt': 1, 'comments': ''}
-
+            if price == price_1:
+                min_q  = min(d[key]['qa'], d[key]['qb'])
+                min_q1 = min(d[key_1]['qa'], d[key_1]['qb'])
+                max_q  = max(d[key]['qa'], d[key]['qb'])
+                max_q1 = max(d[key_1]['qa'], d[key_1]['qb'])
+                if ((max_q - max_q1) == 0) and (name in name_1 or name_1 in name):
+                    print("key: {}\nkey_1: {}\nmax_q: {}\nmax_q1: {}\n\n".format(key, key_1, max_q, max_q1))
+                    # max_qa = max(d[key]['qa'], d[key_1]['qa'])
+                    # max_qb = max(d[key]['qb'], d[key_1]['qb'])
+                    res[key] = {'qnt': 1, 'comments': '', 'qa': 0, 'qb': 0}
+                    # print("found {} - {} with same price/name".format(key, key_1))
+                    to_del = key_1
+                    break
+        if to_del:
+            del cc[to_del]
+            to_del = None
+            
     return res
 
 accs = [
-    ('108.51', 1, 5, '108.51', 5, 12),
+    ('108.52', 1, 5, '108.52', 5, 12),
     # ('108.52', 1, 5, '108.52', 7, 14),
     # ('108.55', 1, 5, '108.55', 7, 14),
     # ('108.56', 1, 5, '108.56', 7, 14),
@@ -86,8 +102,8 @@ for acc in accs:
     c = compare(acc[0], a, b)
     c = fix_doubles(c)
 
-    f.write('{}\t{}\t{}\t{}\t{}\n'.format('счёт','наименование','стоимость','знач','bebebe'))
+    f.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format('счёт','наименование','стоимость','знач','bebebe','qa','qb'))
     for key_c in c:
-        f.write('{}\t{}\t{}\t{}\t{}\n'.format(acc[3],key_c[0], str(key_c[1]).replace('.',','), c[key_c]['qnt'], c[key_c]['comments']))
+        f.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(acc[3],key_c[0], str(key_c[1]).replace('.',','), c[key_c]['qnt'], c[key_c]['comments'], c[key_c]['qa'], c[key_c]['qb']))
 
 f.close()
